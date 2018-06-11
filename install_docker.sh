b0eeb7c1522f55b8dda1f02ad2337c3a4e09cd6c
David Carboni <david.carboni.ext@bbc.co.uk>
2018-06-11 22:01:26 +0100

    Convenience script to install Docker.

---
+++ install_docker.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+sudo apt update
+sudo apt-get install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   stable"
+sudo apt update
+sudo apt install docker-ce
+sudo groupadd docker
+sudo usermod -aG docker $USER
