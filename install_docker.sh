475f2b5ee0811ac11465200afc09cfae4e4e9f0f
David Carboni <davidcarboni@users.noreply.github.com>
2018-06-12 12:05:50 +0100

    Go ahead with installation without prompting.

--- install_docker.sh
+++ install_docker.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 sudo apt update
-sudo apt-get install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+sudo apt-get -y install apt-transport-https ca-certificates curl gnupg2 software-properties-common
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository \
@@ -9,6 +9,6 @@ sudo add-apt-repository \
    $(lsb_release -cs) \
    stable"
 sudo apt update
-sudo apt install docker-ce
+sudo apt -y install docker-ce
 sudo groupadd docker
 sudo usermod -aG docker $USER
